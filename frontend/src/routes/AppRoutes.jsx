cfc499179340da9a53512920ddcaafb64eb44454
Heeralal Kumar Heera <heeralalkumarheera@gmail.com>
2026-01-07 19:38:52 +0530

    testing

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,9 +1,17 @@
-
 import React from "react";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Dashboard from "../pages/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import LeadGroups from "../pages/LeadGroups";
+
 import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => (
@@ -17,44 +25,30 @@ const AppRoutes = () => (
         </ProtectedRoute>
       }
     />
+
+    {/* Public Routes */}
+    <Route path="/" element={<Home />} />
+    <Route path="/login" element={<Login />} />
+
+    {/* Protected Routes */}
+    <Route
+      path="/dashboard"
+      element={
+        <ProtectedRoute>
+          <Dashboard />
+        </ProtectedRoute>
+      }
+    />
+
+    <Route
+      path="/dashboard/lead-groups"
+      element={
+        <ProtectedRoute>
+          <LeadGroups />
+        </ProtectedRoute>
+      }
+    />
   </Routes>
 );
-
-import { Routes, Route } from "react-router-dom";
-
-import Home from "../pages/Home";
-import Login from "../pages/Login";
-import Dashboard from "../pages/Dashboard";
-import LeadGroups from "../pages/LeadGroups";
-
-import ProtectedRoute from "./ProtectedRoute";
-
-
-  return (
-    <Routes>
-      {/* Public Routes */}
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-
-      {/* Protected Routes */}
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/dashboard/lead-groups"
-        element={
-          <ProtectedRoute>
-            <LeadGroups />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
-  );
 
 export default AppRoutes;
--- frontend/src/services/authServices.js
+++ frontend/src/services/authServices.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-export const loginUser = (email, password) => {
-  if (email === "admin@crm.com" && password === "123456") {
-    localStorage.setItem("isAuth", "true");
-    return true;
-  }
-  return false;
-};
-
-export const logoutUser = () => {
-  localStorage.removeItem("isAuth");
-};
-
-export const isAuthenticated = () => {
-  return localStorage.getItem("isAuth") === "true";
-=======
 export const loginUser = (data) => {
   if (data.email && data.password) {
     localStorage.setItem("isAuthenticated", "true");
     console.log("Login successful");
   }
   console.log("Login data:", data);
->>>>>>> origin/main
 };
